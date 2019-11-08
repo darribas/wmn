44be095fa8685645f14e084ed2b168231fb70afe
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2019-11-08 12:31:38 +0000

    Consistent terminology around Web Mapping Notes

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -42,7 +42,7 @@ function doSearch() {
   // Loop through, match, and add results
   for (var item in result) {
     var ref = result[item].ref;
-    var searchitem = '<div class="result"><p><a href="/wma'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
+    var searchitem = '<div class="result"><p><a href="/wmn'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
     resultdiv.append(searchitem);
   }
 }
